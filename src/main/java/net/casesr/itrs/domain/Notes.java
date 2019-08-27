3983a1872d765cf258b274be42445fcf6b2d799b
Racquel Cases <racquel.cases@hotmail.com>
2019-08-27 22:06:53 +0800

    Create Recipe and Notes entities and create one-to-one relationship.

---
+++ src/main/java/net/casesr/itrs/domain/Notes.java
@@ -0,0 +1,42 @@
+package net.casesr.itrs.domain;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Lob;
+import javax.persistence.OneToOne;
+
+@Entity
+public class Notes {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	@OneToOne
+	private Recipe recipe;
+	
+	@Lob
+	private String description;
+	
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public Recipe getRecipe() {
+		return recipe;
+	}
+	public void setRecipe(Recipe recipe) {
+		this.recipe = recipe;
+	}
+	public String getDescription() {
+		return description;
+	}
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+}
