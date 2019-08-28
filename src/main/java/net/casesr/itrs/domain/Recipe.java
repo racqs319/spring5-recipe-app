86c575ba19395381cee4c97c1aaf9c46eccd1ba2
Racquel Cases <racquel.cases@hotmail.com>
2019-08-28 18:27:15 +0800

    Create Ingredient model and create one-to-many relationship with Recipe model.

--- src/main/java/net/casesr/itrs/domain/Recipe.java
+++ src/main/java/net/casesr/itrs/domain/Recipe.java
@@ -1,11 +1,14 @@
 package net.casesr.itrs.domain;
 
+import java.util.Set;
+
 import javax.persistence.CascadeType;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Lob;
+import javax.persistence.OneToMany;
 import javax.persistence.OneToOne;
 
 @Entity
@@ -29,6 +32,9 @@ public class Recipe {
     
     @OneToOne(cascade = CascadeType.ALL)
     private Notes notes;
+    
+    @OneToMany(cascade = CascadeType.ALL, mappedBy = "recipe")
+    private Set<Ingredient> ingredients;
     
     public Long getId() {
 		return id;
@@ -89,6 +95,12 @@ public class Recipe {
 	}
 	public void setNotes(Notes notes) {
 		this.notes = notes;
+	}
+	public Set<Ingredient> getIngredients() {
+		return ingredients;
+	}
+	public void setIngredients(Set<Ingredient> ingredients) {
+		this.ingredients = ingredients;
 	}
 
 }
