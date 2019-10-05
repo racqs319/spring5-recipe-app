636d7f4cdf9a5974432d6afc9dd443302fa2bd1b
Racquel Cases <racquel.cases@hotmail.com>
2019-10-05 19:28:57 +0800

    Add functionality to save image to database.

--- src/main/java/net/casesr/recipeapp/services/impl/ImageServiceImpl.java
+++ src/main/java/net/casesr/recipeapp/services/impl/ImageServiceImpl.java
@@ -1,16 +1,48 @@
 package net.casesr.recipeapp.services.impl;
 
 import lombok.extern.slf4j.Slf4j;
+import net.casesr.recipeapp.domain.Recipe;
+import net.casesr.recipeapp.repositories.RecipeRepository;
 import net.casesr.recipeapp.services.ImageService;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+import java.util.Optional;
 
 @Slf4j
 @Service
 public class ImageServiceImpl implements ImageService {
 
+    private final RecipeRepository recipeRepository;
+
+    public ImageServiceImpl(RecipeRepository recipeRepository) {
+        this.recipeRepository = recipeRepository;
+    }
+
     @Override
     public void saveImageFile(Long recipeId, MultipartFile file) {
         log.debug("Received a file");
+
+        try{
+            Recipe recipe = recipeRepository.findById(recipeId).get();
+
+            Byte[] byteObject = new Byte[file.getBytes().length];
+
+            int i = 0;
+
+            for (byte b : file.getBytes()) {
+                byteObject[i++] = b;
+            }
+
+            recipe.setImage(byteObject);
+
+            recipeRepository.save(recipe);
+        } catch (IOException e) {
+            //todo handle better
+            log.error("Error occurred", e);
+
+            e.printStackTrace();
+        }
     }
 }
