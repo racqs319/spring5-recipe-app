9c68196242449bd47d7815e6ef2827230a91dbc2
Racquel Cases <racquel.cases@hotmail.com>
2019-10-05 21:25:55 +0800

    Add functionality to display image from database.

--- src/main/java/net/casesr/recipeapp/controllers/ImageController.java
+++ src/main/java/net/casesr/recipeapp/controllers/ImageController.java
@@ -1,8 +1,10 @@
 package net.casesr.recipeapp.controllers;
 
 import lombok.extern.slf4j.Slf4j;
+import net.casesr.recipeapp.commands.RecipeCommand;
 import net.casesr.recipeapp.services.ImageService;
 import net.casesr.recipeapp.services.RecipeService;
+import org.apache.tomcat.util.http.fileupload.IOUtils;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -10,6 +12,11 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.multipart.MultipartFile;
+
+import javax.servlet.http.HttpServletResponse;
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.io.InputStream;
 
 @Slf4j
 @Controller
@@ -36,5 +43,24 @@ public class ImageController {
         imageService.saveImageFile(Long.valueOf(recipeId), file);
 
         return "redirect:/recipe/" + recipeId + "/show";
+    }
+
+    @GetMapping("/recipe/{recipeId}/recipeimage")
+    public void renderImageFromDB(@PathVariable String recipeId, HttpServletResponse response) throws IOException {
+        RecipeCommand recipeCommand = recipeService.findCommandById(Long.valueOf(recipeId));
+
+        if (recipeCommand.getImage() != null) {
+            byte[] byteArray = new byte[recipeCommand.getImage().length];
+
+            int i = 0;
+
+            for (Byte wrappedByte : recipeCommand.getImage()) {
+                byteArray[i++] = wrappedByte;
+            }
+
+            response.setContentType("image/jpeg");
+            InputStream is = new ByteArrayInputStream(byteArray);
+            IOUtils.copy(is, response.getOutputStream());
+        }
     }
 }
--- src/main/java/net/casesr/recipeapp/converters/RecipeToRecipeCommand.java
+++ src/main/java/net/casesr/recipeapp/converters/RecipeToRecipeCommand.java
@@ -40,6 +40,7 @@ public class RecipeToRecipeCommand implements Converter<Recipe, RecipeCommand> {
         recipeCommand.setServings(source.getServings());
         recipeCommand.setSource(source.getSource());
         recipeCommand.setUrl(source.getUrl());
+        recipeCommand.setImage(source.getImage());
         recipeCommand.setNotes(notesConverter.convert(source.getNotes()));
 
         if (source.getCategories() != null && !source.getCategories().isEmpty()) {
