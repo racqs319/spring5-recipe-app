43cf5b4932d35cb4698acd640f1c6412ae4dbf8e
Racquel Cases <racquel.cases@hotmail.com>
2019-10-05 18:43:20 +0800

    Add functionality to upload image - frontend.

---
+++ src/main/java/net/casesr/recipeapp/services/ImageService.java
@@ -0,0 +1,9 @@
+package net.casesr.recipeapp.services;
+
+import org.springframework.web.multipart.MultipartFile;
+
+public interface ImageService {
+
+    void saveImageFile(Long recipeId, MultipartFile file);
+
+}
